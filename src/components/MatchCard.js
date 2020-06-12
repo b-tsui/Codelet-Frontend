11ebc7f38475438b846648f8f10de9a29e20b641
Lisa Kang <lleekkang@gmail.com>
2020-06-11 23:32:24 -0400

    Fix matching game to get closer to end goal

--- src/components/MatchCard.js
+++ src/components/MatchCard.js
@@ -1,88 +1,105 @@
-import React, {useState, useEffect} from 'react'
-import Card from '@material-ui/core/Card';
+import React, { useState, useEffect } from "react";
+import Card from "@material-ui/core/Card";
 
-export default function MatchCard({card, props}){
-    const { setSelectedDef,
-        setSelectedTerm,
-        setIsTwoSelected,
-        isTwoSelected,
-        selectedDef,
-        selectedTerm,
-        isMatched,
-        setIsMatched} = props;
+export default function MatchCard({ card, props }) {
+  const {
+    setSelectedDef,
+    setSelectedTerm,
+    setIsTwoSelected,
+    isTwoSelected,
+    selectedDef,
+    selectedTerm,
+    isMatched,
+    setIsMatched,
+  } = props;
 
-    const [isSelected, setIsSelected] = React.useState(false);
-    const [opaqueValue, setOpaqueValue] = React.useState(1);
-    const [termColor, setTermColor] = React.useState('lightgray');
-    const [defColor, setDefColor] = React.useState('gray');
+  const [isSelected, setIsSelected] = React.useState(false);
+  const [opaqueValue, setOpaqueValue] = React.useState(1);
+  // const [termColor, setTermColor] = React.useState('lightgray');
+  // const [defColor, setDefColor] = React.useState('gray');
 
-    useEffect(() => {
-        const opaqueCard = async () => {
-            if (isMatched && isSelected) {
-                setOpaqueValue(0)
+  useEffect(() => {
+    const opaqueCard = async () => {
+      if (isMatched && isSelected) {
+        setOpaqueValue(0);
+        // debugger;
+      }
+      if (!isMatched && isSelected) {
+        setIsSelected(false);
+        // debugger;
+      }
+    };
+    opaqueCard();
+  }, [isMatched]);
 
-            }
-        }
-        opaqueCard();
-    }, [isMatched])
+  // useEffect(() => {
+  //     const resetSelection = async ()=>{
+  //         if (isTwoSelected && !isMatched) {
+  //             setIsSelected(false)
+  //             setTermColor('lightgray')
+  //             setDefColor('gray')
+  //         }
+  //     }
+  //     resetSelection()
+  // }, [isTwoSelected])
 
-    // useEffect(() => {
-    //     const resetSelection = async ()=>{
-    //         if (isTwoSelected && !isMatched) {
-    //             setIsSelected(false)
-    //             setTermColor('lightgray')
-    //             setDefColor('gray')
-    //         }
-    //     }
-    //     resetSelection()
-    // }, [isTwoSelected])
-
-    const handleTermClick = async (e) => {
-        const idCard = Number(e.target.id.slice(5));
-        setIsSelected(true)
-        setTermColor('yellow')
-        setSelectedTerm(idCard);
-        if (selectedDef) {
-            setIsTwoSelected(true)
-        }
-        // if (isTwoSelected && !isMatched) {
-        //     setIsSelected(false)
-        // }
+  const handleTermClick = async (e) => {
+    const idCard = Number(e.target.id.slice(5));
+    setIsSelected(true);
+    // setTermColor("yellow");
+    setSelectedTerm(idCard);
+    if (selectedDef) {
+      setIsTwoSelected(true);
     }
+    // if (isTwoSelected && !isMatched) {
+    //     setIsSelected(false)
+    // }
+  };
 
-    const handleDefClick = async (e) => {
-        const idCard = Number(e.target.id.slice(4));
-        setIsSelected(true)
-        setDefColor('yellow')
-        setSelectedDef(idCard);
-        if (selectedTerm) {
-            setIsTwoSelected(true);
-        }
-        // if (isTwoSelected && !isMatched) {
-        //     setIsSelected(false)
-        // }
-
-
+  const handleDefClick = async (e) => {
+    const idCard = Number(e.target.id.slice(4));
+    setIsSelected(true);
+    // setDefColor("yellow");
+    setSelectedDef(idCard);
+    if (selectedTerm) {
+      setIsTwoSelected(true);
     }
-    // color: isUpvoted === false ? "#e57373" : "#eeeeee"
-    if (card.term) {
-        return <Card className="random-cards random-term"
-            style={{ 'overflow-y': 'auto',
-            backgroundColor: termColor,
-            opacity: opaqueValue
-             }}
-
-            id={`term-${card.cardId}`}
-            onClick={handleTermClick}
-        >{card.term}</Card>
-    } else {
-        return <Card className="random-cards random-def"
-            style={{ 'overflow-y': 'auto',
-                backgroundColor: defColor,
-                opacity: opaqueValue
-                }}
-            id={`def-${card.cardId}`}
-            onClick={handleDefClick}
-        >{card.definition}</Card>
-    }
+    // if (isTwoSelected && !isMatched) {
+    //     setIsSelected(false)
+    // }
+  };
+  // color: isUpvoted === false ? "#e57373" : "#eeeeee"
+  if (card.term) {
+    return (
+      <Card
+        className="random-cards random-term"
+        style={{
+          "overflow-y": "auto",
+          backgroundColor: isSelected ? "yellow" : "lightgray",
+          // backgroundColor: termColor,
+          opacity: opaqueValue,
+        }}
+        id={`term-${card.cardId}`}
+        onClick={handleTermClick}
+      >
+        {card.term}
+      </Card>
+    );
+  } else {
+    return (
+      <Card
+        className="random-cards random-def"
+        style={{
+          "overflow-y": "auto",
+          backgroundColor: isSelected ? "yellow" : "gray",
+          // backgroundColor: defColor,
+          opacity: opaqueValue,
+        }}
+        id={`def-${card.cardId}`}
+        onClick={handleDefClick}
+      >
+        {card.definition}
+      </Card>
+    );
+  }
 }
