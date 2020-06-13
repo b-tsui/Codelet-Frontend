1a6c53de35302b8894ed6f2706318dbc7a3bf96e
Lisa Kang <lleekkang@gmail.com>
2020-06-13 00:48:11 -0400

    Style selected div on side navbar

--- src/components/SideBarBrowse.js
+++ src/components/SideBarBrowse.js
@@ -17,129 +17,129 @@ import Typography from "@material-ui/core/Typography";
 import HomeIcon from "@material-ui/icons/Home";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        width: "100%",
-    },
-    list: {
-        width: 250,
-    },
-    fullList: {
-        width: "auto",
-    },
-    heading: {
-        fontSize: theme.typography.pxToRem(15),
-        fontWeight: theme.typography.fontWeightRegular,
-    },
-    paper: {
-        background: "#000a12",
-        //color: 'white'
-    },
-    divider: {
-        color: "secondary",
-    },
+  root: {
+    width: "100%",
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: "auto",
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  paper: {
+    background: "#000a12",
+    //color: 'white'
+  },
+  divider: {
+    color: "secondary",
+  },
 }));
 
 export default function SideBarBrowse() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [state, setState] = React.useState({
-        top: false,
-        left: false,
-        bottom: false,
-        right: false,
-    });
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
 
-    const [selectedIndex, setSelectedIndex] = React.useState(0);
-    const categories = [
-        "Data Structures",
-        "Algorithms",
-        "Javascript",
-        "Python",
-        "Databases",
-        "Frontend",
-        "Backend",
-    ];
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const categories = [
+    "Data Structures",
+    "Algorithms",
+    "Javascript",
+    "Python",
+    "Databases",
+    "Frontend",
+    "Backend",
+  ];
 
-    const handleListItemClick = (event, index) => {
-        setSelectedIndex(index);
-    };
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
 
-    const toggleDrawer = (anchor, open) => (event) => {
-        if (
-            event &&
-            event.type === "keydown" &&
-            (event.key === "Tab" || event.key === "Shift")
-        ) {
-            return;
-        }
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
 
-        setState({ ...state, [anchor]: open });
-    };
+    setState({ ...state, [anchor]: open });
+  };
 
-    const list = (anchor) => (
-        <div
-            className={clsx(classes.list, {
-                [classes.fullList]: anchor === "top" || anchor === "bottom",
-            })}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
-        >
-            <List component="nav" aria-label="main mailbox folders">
-                <Link to="/">
-                    <ListItem
-                        button
-                        selected={selectedIndex === 0}
-                        onClick={(event) => handleListItemClick(event, 0)}
-                    >
-                        <ListItemIcon id="browse-home-icon">
-                            <HomeIcon color="primary" />
-                        </ListItemIcon>
-                        <ListItemText id="browse-home" primary="Home" />
-                    </ListItem>
-                </Link>
-            </List>
-            <Divider id="browse-divider-top" />
-            <Typography variant="h5" id="browse-nav-title">
-                Categories
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === "top" || anchor === "bottom",
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List component="nav" aria-label="main mailbox folders">
+        <Link to="/">
+          <ListItem
+            button
+            selected={selectedIndex === 0}
+            onClick={(event) => handleListItemClick(event, 0)}
+          >
+            <ListItemIcon id="browse-home-icon">
+              <HomeIcon color={selectedIndex === 0 ? "lightgray" : "primary"} />
+            </ListItemIcon>
+            <ListItemText id="browse-home" primary="Home" />
+          </ListItem>
+        </Link>
+      </List>
+      <Divider id="browse-divider-top" />
+      <Typography variant="h5" id="browse-nav-title">
+        Categories
       </Typography>
-            <Divider id="browse-diverder-bottom" />
-            <List component="nav" aria-label="secondary mailbox folder">
-                {categories.map((text, i) => (
-                    <Link to={`/categories/${i + 1}`}>
-                        <ListItem
-                            key={text}
-                            button
-                            selected={selectedIndex === i + 1}
-                            onClick={(event) => handleListItemClick(event, i + 1)}
-                        >
-                            <ListItemText className="browse-item" primary={text} />
-                        </ListItem>
-                    </Link>
-                ))}
-            </List>
-        </div>
-    );
+      <Divider id="browse-diverder-bottom" />
+      <List component="nav" aria-label="secondary mailbox folder">
+        {categories.map((text, i) => (
+          <Link to={`/categories/${i + 1}`}>
+            <ListItem
+              key={text}
+              button
+              selected={selectedIndex === i + 1}
+              onClick={(event) => handleListItemClick(event, i + 1)}
+            >
+              <ListItemText className="browse-item" primary={text} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </div>
+  );
 
-    return (
-        <div>
-            <>
-                <Button
-                    onClick={toggleDrawer("left", true)}
-                    style={{ color: "#e8eaf6" }}
-                >
-                    <MenuIcon />
-                </Button>
-                <SwipeableDrawer
-                    anchor={"left"}
-                    open={state["left"]}
-                    onClose={toggleDrawer("left", false)}
-                    onOpen={toggleDrawer("left", true)}
-                    classes={{ paper: classes.paper }}
-                >
-                    {list("left")}
-                </SwipeableDrawer>
-            </>
-        </div>
-    );
+  return (
+    <div>
+      <>
+        <Button
+          onClick={toggleDrawer("left", true)}
+          style={{ color: "#e8eaf6" }}
+        >
+          <MenuIcon />
+        </Button>
+        <SwipeableDrawer
+          anchor={"left"}
+          open={state["left"]}
+          onClose={toggleDrawer("left", false)}
+          onOpen={toggleDrawer("left", true)}
+          classes={{ paper: classes.paper }}
+        >
+          {list("left")}
+        </SwipeableDrawer>
+      </>
+    </div>
+  );
 }
